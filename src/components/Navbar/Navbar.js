df3391e762dc1d2ce54c1c7ed52eff0f677fa213
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-06-01 13:37:14 -0300

    Desktop styling and refactor, menu functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
 import { InlineIcon } from "@iconify/react-with-api"
 import { StaticImage } from "gatsby-plugin-image"
 
 import "./navbar.scss"
+import { Link } from "gatsby"
 
 const Navbar = () => {
+  const [displayNavbar, setDisplayNavbar] = useState(false)
+
   return (
     <nav className="navbar">
       <nav className="navbar__navbar">
-        <button className="btn">
-          <InlineIcon icon="system-uicons:menu-hamburger" />
+        <button
+          className="navbar__btn"
+          onClick={() => setDisplayNavbar(!displayNavbar)}
+        >
+          <InlineIcon
+            icon={
+              displayNavbar
+                ? "system-uicons:close"
+                : "system-uicons:menu-hamburger"
+            }
+          />
+          <span>menu</span>
         </button>
+        <ul className={`navbar__menu ${displayNavbar ? "d-block" : ""}`}>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about_us">about us</Link>
+          </li>
+          <li>
+            <Link to="/works">works</Link>
+          </li>
+          <li>
+            <Link to="/our_team">our team</Link>
+          </li>
+          <li>
+            <Link to="/find_us">find us</Link>
+          </li>
+        </ul>
       </nav>
       <div className="navbar__logo">
-        <StaticImage src="../../images/logo.png" />
+        <StaticImage src="../../images/logo.png" alt="AIRhitect logo" />
       </div>
-      <button className="btn navbar__contact__button">contact</button>
+      <Link to="/find_us" className="btn navbar__contact__button">
+        <InlineIcon icon="carbon:user-avatar" /> contact
+      </Link>
     </nav>
   )
 }
