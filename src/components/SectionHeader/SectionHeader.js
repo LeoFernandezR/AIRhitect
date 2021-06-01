6279e3b0a3cfb2a86b33a6937aab2819d2854fb3
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-06-01 13:39:17 -0300

    Section header component

---
+++ src/components/SectionHeader/SectionHeader.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import "./sectionheader.scss"
+
+const SectionHeader = ({ children }) => {
+  return <h1 className="section__header">{children}</h1>
+}
+
+export default SectionHeader
